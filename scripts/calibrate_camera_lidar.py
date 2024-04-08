d4fc6c572f17d1c65f974bb8b4b40f5af51a4ab0
man.tmm <155959855+mantruong204@users.noreply.github.com>
2024-04-08 17:08:38 +0700

    Update calibrate_camera_lidar.py

--- scripts/calibrate_camera_lidar.py
+++ scripts/calibrate_camera_lidar.py
@@ -93,7 +93,7 @@ Outputs: None
 '''
 def handle_keyboard():
     global KEY_LOCK, PAUSE
-    key = raw_input('Press [ENTER] to pause and pick points\n')
+    key = input('Press [ENTER] to pause and pick points\n')
     with KEY_LOCK: PAUSE = True
 
 
@@ -233,10 +233,10 @@ def extract_points_3D(velodyne, now):
     if OUSTER_LIDAR: points = points.reshape(-1, 9)[:, :4]
 
     # Select points within chessboard range
-    inrange = np.where((points[:, 0] > 0) &
-                       (points[:, 0] < 2.5) &
-                       (np.abs(points[:, 1]) < 2.5) &
-                       (points[:, 2] < 2))
+    inrange = np.where((points[:, 0] > -1) &   #horizonal FOV
+                       (points[:, 0] < 1.5) &
+                       (np.abs(points[:, 0]) < 5) &
+                       (points[:, 2] < 3))
     points = points[inrange[0]]
     print(points.shape)
     if points.shape[0] > 5:
@@ -337,7 +337,7 @@ def calibrate(points2D=None, points3D=None):
         translation_vector, camera_matrix, dist_coeffs)[0].squeeze(1)
     assert(points2D_reproj.shape == points2D.shape)
     error = (points2D_reproj - points2D)[inliers]  # Compute error only over inliers.
-    rmse = np.sqrt(np.mean(error[:, 0] ** 2 + error[:, 1] ** 2))
+    rmse = np.sqrt(np.mean(error[:, 0,0] ** 2 + error[:,0, 1] ** 2))
     rospy.loginfo('Re-projection error before LM refinement (RMSE) in px: ' + str(rmse))
 
     # Refine estimate using LM
@@ -355,7 +355,7 @@ def calibrate(points2D=None, points3D=None):
             translation_vector, camera_matrix, dist_coeffs)[0].squeeze(1)
         assert(points2D_reproj.shape == points2D.shape)
         error = (points2D_reproj - points2D)[inliers]  # Compute error only over inliers.
-        rmse = np.sqrt(np.mean(error[:, 0] ** 2 + error[:, 1] ** 2))
+        rmse = np.sqrt(np.mean(error[:,0, 0] ** 2 + error[:,0, 1] ** 2))
         rospy.loginfo('Re-projection error after LM refinement (RMSE) in px: ' + str(rmse))
 
     # Convert rotation vector
@@ -531,9 +531,9 @@ if __name__ == '__main__':
 
     # Calibration mode, rosrun
     if sys.argv[1] == '--calibrate':
-        camera_info = '/sensors/camera/camera_info'
-        image_color = '/sensors/camera/image_color'
-        velodyne_points = '/sensors/velodyne_points'
+        camera_info = '/webcam_publisher/camera/camera_info'
+        image_color = '/webcam_publisher/camera/image_rect_color'
+        velodyne_points = '/velodyne_points'
         camera_lidar = None
         PROJECT_MODE = False
     # Projection mode, run from launch file
